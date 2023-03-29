1f96306038e397a503a8a7b1531892c382a5a016
Merless-as <84011308+Merless-as@users.noreply.github.com>
2023-03-29 11:31:51 +0200

    Add files via upload
    Add CreateStructure.sql

---
+++ CreateStructure.sql
@@ -0,0 +1,24 @@
+CREATE TABLE dbo.SKU  
+(
+    ID INT IDENTITY(1, 1) PRIMARY KEY, 
+    Code AS ('s'+ CAST(ID AS VARCHAR(12))) UNIQUE, 
+    Name VARCHAR(20)
+);
+
+CREATE TABLE dbo.Family 
+(
+    ID INT IDENTITY(1, 1) PRIMARY KEY, 
+    SurName VARCHAR(20), 
+    BudgetValue MONEY
+);
+
+CREATE TABLE dbo.Basket 
+(
+    ID INT IDENTITY(1, 1), 
+    ID_SKU INT FOREIGN KEY REFERENCES dbo.SKU(ID), 
+    ID_Family INT FOREIGN KEY REFERENCES dbo.Family(ID), 
+    Quantity FLOAT CHECK (Quantity >= 0), 
+    Value FLOAT CHECK (Value >= 0), 
+    PurchaseDate DATE DEFAULT (SYSDATETIME()), 
+    DiscountValue FLOAT
+);
