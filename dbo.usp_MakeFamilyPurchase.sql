3cdc89e5cde51dc4a9743a0fc275ac71822e7911
Merless-as <84011308+Merless-as@users.noreply.github.com>
2023-03-31 20:52:20 +0200

    Update dbo.usp_MakeFamilyPurchase.sql
    Procedure work fixed

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -5,15 +5,17 @@ CREATE PROC MakeFamilyPurchase (@FamilySurName varchar(255))
         FROM dbo.Family
         )
     )
-        PRINT N'No such dbo.Family.SurName exists';
+        BEGIN
+            RAISERROR (N'No such dbo.Family.SurName exists', 
+            10, 
+            1
+            );
+        END;
     ELSE 
         UPDATE dbo.Family 
         SET BudgetValue = (
-            SELECT SUM(Value) 
-            FROM dbo.Basket
-            WHERE dbo.Basket.ID_Family=(
-                SELECT ID 
-                FROM dbo.Family
-                WHERE SurName=@FamilySurName
-            )
-    );
+            SELECT SUM(dbo.Basket.Value) 
+            FROM dbo.Basket, dbo.Family 
+            WHERE dbo.Family.SurName=@FamilySurName
+        )
+        WHERE SurName=@FamilySurName;
