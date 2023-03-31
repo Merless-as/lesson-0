255dd029ded38ec6412275eb651c10141e4da47b
Merless-as <84011308+Merless-as@users.noreply.github.com>
2023-03-31 20:54:51 +0200

    Update dbo.TR_Basket_insert_update.sql
    Trigger work fixed

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -2,20 +2,37 @@ CREATE TRIGGER TR_Basket_insert_update ON dbo.Basket
 AFTER INSERT
 AS
 
-UPDATE dbo.Basket
-SET DiscountValue = Value * 0.05
-WHERE  ID_SKU IN (
-                    SELECT  ID_SKU
-                    FROM inserted
-                    GROUP BY ID_SKU
-                    HAVING COUNT(Value) > 1
-                );
+DECLARE @ROWS INT;
+SET @ROWS = @@ROWCOUNT;
 
-UPDATE dbo.Basket
-SET DiscountValue = 0
-WHERE  ID_SKU IN (
-                    SELECT  ID_SKU
-                    FROM inserted
-                    GROUP BY ID_SKU
-                    HAVING COUNT(Value) < 2
-                );
+IF EXISTS ( 
+            SELECT  ID_SKU
+            FROM inserted
+            GROUP BY ID_SKU
+            HAVING COUNT(Value) > 1
+            )
+    BEGIN
+    UPDATE dbo.Basket
+    SET DiscountValue = Value * 0.05
+    WHERE ID IN (
+                SELECT TOP (@ROWS) ID
+                FROM dbo.Basket 
+                ORDER BY ID DESC
+            )
+            AND ID_SKU IN (
+                SELECT  ID_SKU
+                FROM inserted
+                GROUP BY ID_SKU
+                HAVING COUNT(Value) > 1
+            );
+    END;
+ELSE
+    BEGIN 
+    UPDATE dbo.Basket
+    SET DiscountValue = 0
+    WHERE ID IN (
+                SELECT TOP (@ROWS) ID
+                FROM dbo.Basket 
+                ORDER BY ID DESC
+            );
+    END;
