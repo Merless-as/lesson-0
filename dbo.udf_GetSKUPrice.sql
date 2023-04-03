c912439a3f19a20beab1d650e9b979969cf1964e
Merless-as <84011308+Merless-as@users.noreply.github.com>
2023-04-03 12:35:12 +0200

    Update dbo.udf_GetSKUPrice.sql
    Return type change

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -2,8 +2,10 @@ CREATE FUNCTION GetSKUPrice ( @ID_SKU AS INT )
 RETURNS TABLE
 AS
 
-RETURN (
-    SELECT CAST(b.Value/b.Quantity AS DECIMAL(18, 2)) as Price
+BEGIN
+    DECLARE @Price DECIMAL(18, 2);
+    SELECT @Price = SUM(b.Value/b.Quantity)
     FROM dbo.Basket as b
-    WHERE b.ID_SKU=@ID_SKU
-);
+    WHERE b.ID_SKU=@ID_SKU;
+    RETURN @Price;
+END;
