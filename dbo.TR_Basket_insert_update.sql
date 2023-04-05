4b2eb89dd1f264fcc992846eb6fa5081eb1117ec
Merless-as <84011308+Merless-as@users.noreply.github.com>
2023-04-05 12:59:45 +0200

    Update dbo.TR_Basket_insert_update.sql
    Version 2 added

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -36,3 +36,30 @@ ELSE
                 ORDER BY ID DESC
             );
     END;
+    
+   
+   
+   
+-- version 2
+
+CREATE TRIGGER TR_Basket_insert_update ON dbo.Basket
+AFTER INSERT
+AS
+
+UPDATE dbo.Basket
+SET DiscountValue = (
+    CASE 
+        WHEN EXISTS ( 
+            SELECT  ID_SKU
+            FROM inserted
+            GROUP BY ID_SKU
+            HAVING COUNT(Value) > 1
+            )
+            Then Value * 0.05
+        ELSE 0
+    END
+)
+WHERE ID_SKU IN (
+        SELECT ID_SKU
+        FROM  inserted
+);
