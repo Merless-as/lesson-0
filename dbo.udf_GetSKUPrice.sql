76c3b4c6257499c37c1f3cb072ac0e0d1c3a54a4
Merless-as <84011308+Merless-as@users.noreply.github.com>
2023-04-25 19:23:52 +0200

    Update dbo.udf_GetSKUPrice.sql

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -4,9 +4,11 @@ AS
 
 BEGIN
     DECLARE @Price DECIMAL(18, 2);
+    
     SELECT 
         @Price = SUM(b.Value/b.Quantity)
-        FROM dbo.Basket as b
-        WHERE b.ID_SKU=@ID_SKU;
+    FROM dbo.Basket as b
+    WHERE b.ID_SKU=@ID_SKU;
+    
     RETURN @Price;
 END;
