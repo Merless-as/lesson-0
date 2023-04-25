7407f9f78c524160c776972bf18c47e57c8a4130
Merless-as <84011308+Merless-as@users.noreply.github.com>
2023-04-25 19:19:08 +0200

    Update dbo.usp_MakeFamilyPurchase.sql

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,27 +1,30 @@
 CREATE PROC MakeFamilyPurchase (@FamilySurName varchar(255))
     AS
     IF (@FamilySurName NOT IN (
-        SELECT SurName
+        SELECT 
+            SurName
         FROM dbo.Family
         )
     )
         BEGIN
-            RAISERROR (N'No such dbo.Family.SurName exists', 
-            10, 
-            1
+            RAISERROR (N'No such dbo.Family.SurName exists'
+                       ,10
+                       ,1
             );
         END;
     ELSE 
         Begin
             WITH cte AS (
-                SELECT ID_Family, SUM(Value) as sum
+                SELECT 
+                    ID_Family
+                    ,SUM(Value) as sum
                 FROM dbo.Basket
                 WHERE SurName=@FamilySurName
                 GROUP BY ID_Family
             )
-            UPDATE Fam 
+            UPDATE 
+                Fam 
             SET BudgetValue -= cte.sum 
             FROM dbo.Family as Fam 
-            JOIN cte 
-            ON Fam.ID=cte.ID_Family;
+                JOIN cte ON Fam.ID=cte.ID_Family;
         END;
